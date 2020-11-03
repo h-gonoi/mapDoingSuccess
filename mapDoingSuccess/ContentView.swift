22cefcacef3a2d39bf1400f441aa6a471aa0f4ed
h-gonoi <ginpoke@gmail.com>
2020-11-03 19:27:59 +0900

    a

--- mapDoingSuccess/ContentView.swift
+++ mapDoingSuccess/ContentView.swift
@@ -3,14 +3,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            //下部のタブ
             TabView {
                 taskView()
+                    //タスク画面遷移
                     .tabItem {
                        Image(systemName: "list.dash")
                         Text("TASK")
                     }.tag(1)
                     .accentColor(.green)
                 mapView()
+                    //マップ画面遷移
                     .tabItem {
                         Image(systemName: "map.fill")
                         Text("MAP")
